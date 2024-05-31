6cea65383c12d3fe69f5c542a71e431a250ef72d
Renan Franca <renan.andradefranca@gmail.com>
2024-05-31 14:51:19 -0300

    Apply frontend-maven-plugin module

---
+++ src/main/java/tech/jhipster/sampleapp/wire/frontend/infrastructure/primary/RedirectionResource.java
@@ -0,0 +1,29 @@
+package tech.jhipster.sampleapp.wire.frontend.infrastructure.primary;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import tech.jhipster.sampleapp.shared.generation.domain.ExcludeFromGeneratedCodeCoverage;
+
+@Controller
+@ExcludeFromGeneratedCodeCoverage
+class RedirectionResource {
+
+  private static final String FORBID_EXTENSION_PATTERN = "/{file:[^\\.]*}";
+  private static final String TRAILING_DIR_PATTERN = "/{trailingDir:.*}/";
+
+  @GetMapping(
+    value = {
+      FORBID_EXTENSION_PATTERN,
+      "/*" + FORBID_EXTENSION_PATTERN,
+      "/*/*" + FORBID_EXTENSION_PATTERN,
+      TRAILING_DIR_PATTERN,
+      "/*" + TRAILING_DIR_PATTERN,
+      "/*/*" + TRAILING_DIR_PATTERN,
+    }
+  )
+  public String redirectApi(@PathVariable(required = false) String file, @PathVariable(required = false) String trailingDir) {
+    return "forward:/";
+  }
+}
